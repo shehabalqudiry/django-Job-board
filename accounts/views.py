9d040f9dfc5541de8cb95c079a920f170835085a
shehabalqudiry <omran.shehab40@gmail.com>
2020-08-04 20:50:23 +0200

    .

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .forms import SignupForm
 
 # Create your views here.
+
+
+def signup(request):
+    form = SignupForm()
+    context = {'form': form}
+    return render(request, 'registration/signup.html', context)
