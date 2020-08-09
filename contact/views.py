1139d87da943a19b70952a0f1e90bf9d9f065dac
shehabalqudiry <omran.shehab40@gmail.com>
2020-08-09 14:51:51 +0200

    add Contact us

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-
+from .models import Info
+from django.core.mail import send_mail
+from django.conf import settings
 # Create your views here.
 def contact(request):
-    
-    return render(request, 'contact.html', {'title':'contact'})
+    info = Info.objects.first()
+    if request.method == 'POST':
+        name = request.POST['name']
+        message = request.POST['message']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        send_mail(
+            subject,
+            message,
+            email,
+            [settings.EMAIL_HOST_USER],
+        )
+    context = {'info':info}
+    return render(request, 'contact/contact.html', context)
